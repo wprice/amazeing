e3bf0ce8064c8972ab1ed1bad1b443c736a6ae27
Weston M. Price <weston.price@gmail.com>
2017-04-21 21:21:54 -0700

    Travis CI
    - Adding travis.yml to automate tests etc

--- src/test/java/price/weston/amazeing/MazeFactoryTest.java
+++ src/test/java/price/weston/amazeing/MazeFactoryTest.java
@@ -1,12 +1,21 @@
 package price.weston.amazeing;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Created by wprice on 4/21/17.
  */
 public class MazeFactoryTest {
 
+    private static final Logger logger = LoggerFactory.getLogger(MazeFactoryTest.class);
+
+    @Before
+    public void setUp() {
+        logger.info("init");
+    }
     @Test
     public void testMazeFactory() {
         Maze maze = MazeFactory.createMaze();
